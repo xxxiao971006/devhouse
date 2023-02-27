b7422b254fc3c11bfc50a46551a704df0336e192
Xiao Zhang <xxxiao971006@gmail.com>
2023-02-27 12:27:51 -0800

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,41 @@
 import express from "express";
-import cookieParser from "cookie-parser";
 import errorMiddleware from "./middleware/error.middleware";
 import Controller from "./interfaces/controller.interface";
-import path from "path";
+import dotenv from "dotenv";
 
 class App {
-  public app: express.Application;
-  public port: number | string = process.env.PORT || 5000;
+  private _app: express.Application;
+  private readonly _port: number | string = process.env.PORT || 5000;
 
   constructor(controllers: Controller[]) {
-    this.app = express();
+    this._app = express();
+    dotenv.config();
 
-    // this.connectToTheDatabase();
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
     this.initializeErrorHandling();
   }
 
-  public listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
+  public start() {
+    this._app.listen(this._port, () => {
+      console.log(`App listening on the port ${this._port}`);
     });
   }
 
-  public getServer() {
-    return this.app;
-  }
-
   private initializeMiddlewares() {
-    this.app.use(express.static(path.join(__dirname, "public")));
-    this.app.use(express.urlencoded());
-    this.app.use(express.json());
-    this.app.use(cookieParser());
-    this.app.set("views", path.join(__dirname, "views"));
-    this.app.set("view engine", "ejs");
+    require("./middleware/express.middlewares")(this._app);
+    require("./middleware/passport.middlewares")(this._app);
   }
 
   private initializeErrorHandling() {
-    this.app.use(errorMiddleware);
+    this._app.use(errorMiddleware);
   }
 
   private initializeControllers(controllers: Controller[]) {
     controllers.forEach((controller) => {
-      this.app.use("/", controller.router);
+      this._app.use("/", controller.router);
     });
   }
-
-  // private connectToTheDatabase() {
-  //   const {
-  //     MONGO_USER,
-  //     MONGO_PASSWORD,
-  //     MONGO_PATH,
-  //   } = process.env;
-  //   mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
-  // }
 }
 
 export default App;
